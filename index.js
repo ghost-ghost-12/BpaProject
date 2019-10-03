3df61a5fb893847b3e2fb391135b20be3d8dd22a
PS20JSilvestri <PS20JSilvestri@efcts.us>
2019-10-03 14:00:45 -0400

    make it better

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ var server = app.listen(5000, function () {
 });
 */
 
+//.get('/', (req, res) => res.render('home.html'))
+
 
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-app.use(express.static("public"));
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
