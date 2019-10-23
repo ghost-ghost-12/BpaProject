b7585e45fc1ea4ec52d6a927c9e24009cb479626
unknown <PS20MCurnell@efcts.us>
2019-10-23 11:36:31 -0400

    added fields to the form - Michae

--- MongoDB/Product.js
+++ MongoDB/Product.js
@@ -4,9 +4,9 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  var myobj = { name: "Product Two", Picture: "http://mypics.com/allan.jpg", Price: "95.60", QTY: "500", Description: "Stuff" };
+  var myobj = { Name: "Product Two", Picture: "http://mypics.com/allan.jpg", Price: "95.60", QTY: "500", Description: "Stuff" };
  
-  dbo.collection("Products").insertOne(myobj, function(err, res) {
+  dbo.collection("Product").insertOne(myobj, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
     db.close();
