b490c54a540bdce443f6958162c902fe8169217f
PS20JSilvestri <PS20JSilvestri@efcts.us>
2019-12-10 14:14:51 -0500

    working on cart

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ var bodyParser = require('body-parser');
 mongoose.connect('mongodb://dbuser:dbpassword1@ds063180.mlab.com:63180/heroku_3wvd8gzq');
 
 var productRouter = require('./routes/product');
-<<<<<<< HEAD
 var cartRouter = require('./routes/cart');
-=======
 var crudRouter = require('./routes/index_crud');
->>>>>>> 1801a42f395b3ae74fcee123207b695bf70b7dc8
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -23,10 +20,7 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   //.get('/cart', (req, res) => res.render('pages/cart'))
   .use('/product', productRouter)
-<<<<<<< HEAD
   .use('/cart', cartRouter)
 //  .get('/add/:id', (req, res) => res.render('pages/cart'))
-=======
   .use('/crud', crudRouter)
->>>>>>> 1801a42f395b3ae74fcee123207b695bf70b7dc8
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
